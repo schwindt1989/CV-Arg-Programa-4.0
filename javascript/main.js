3e9487fd470b2d1e71fb362662a0c045c30682e2
david <david89sch@gmail.com>
2023-01-11 00:30:36 -0300

    mi commit arp4

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+window. alert('Bienvenido a mi proyecto de cv para Argentina programa 4.0')
+setTimeout(function(){
+    window.location.href = 'https://www.youtube.com/watch?v=0vlfHUaXsDY&ab_channel=profediaz';
+
+},500000);
